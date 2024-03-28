d0a36cfea7d27932c8792847bb3d1e022332d894
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-28 21:54:16 +0400

    Added Person and Teacher class

---
+++ src/main/java/bean/Student.java
@@ -0,0 +1,34 @@
+package bean;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hamidhamidbayli
+ */
+public class Student extends Person{
+    
+    private String schoolName;
+    private double scholarship;
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public double getScholarship() {
+        return scholarship;
+    }
+
+    public void setScholarship(double scholarship) {
+        this.scholarship = scholarship;
+    }
+    
+    
+}
